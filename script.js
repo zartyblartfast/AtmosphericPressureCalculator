a325d4edee26c0cbb82c6c1ac16c1551df8129bd
zartyblartfast <zarty_blartfast@yahoo.com>
2023-06-22 14:25:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -35,16 +35,72 @@ function calculatePressure() {
 }
 
 // Function to generate the visual representation of the atmosphere
-function generateAtmosphereVisualization() {
-  // Generate the visualization of the atmosphere as a column/line
-  // You can implement this visualization using CSS or JavaScript
+function generateAtmosphereVisualization(calculatedPressure) {
+  const ctx = document.getElementById('atmosphere-visualization').getContext('2d');
+
+  // Create the bar chart
+  const atmosphereChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Sea Level', 'Altitude'],
+      datasets: [{
+        label: 'Atmospheric Pressure',
+        data: [seaLevelPressureInput.value, calculatedPressure],
+        backgroundColor: [
+          'rgba(0, 123, 255, 0.8)', // Blue color for sea level
+          'rgba(255, 0, 0, 0.8)'     // Red color for altitude
+        ],
+        borderColor: 'rgba(0, 0, 0, 1)',
+        borderWidth: 1
+      }]
+    },
+    options: {
+      responsive: true,
+      scales: {
+        x: {
+          grid: {
+            display: false
+          }
+        },
+        y: {
+          beginAtZero: true,
+          ticks: {
+            stepSize: 100,
+            callback: function (value) {
+              return value + ' hPa';
+            }
+          }
+        }
+      },
+      plugins: {
+        legend: {
+          display: false
+        }
+      }
+    }
+  });
 }
 
-// Function to generate the chart visualization
-function generateChartVisualization() {
-  // Generate the chart visualization showcasing the relationship between altitude and pressure
-  // You can use a library like Chart.js or implement a custom chart using HTML and JavaScript
-}
+// Function to calculate atmospheric pressure
+function calculatePressure() {
+  // Retrieve user inputs
+  const seaLevelPressure = parseFloat(seaLevelPressureInput.value);
+  const temperatureLapseRate = parseFloat(temperatureLapseRateInput.value);
+  const altitude = parseFloat(altitudeInput.value);
+  const standardTemperatureCelsius = parseFloat(standardTemperatureInput.value);
+
+  // Convert Celsius temperature to Kelvin
+  const standardTemperatureKelvin = standardTemperatureCelsius + 273.15;
+
+  // Calculate pressure using the provided formula
+  const calculatedPressure = seaLevelPressure * Math.exp((-1 * altitude) / (temperatureLapseRate * standardTemperatureKelvin));
+
+  // Update the calculated pressure output
+  calculatedPressureOutput.textContent = calculatedPressure.toFixed(2) + ' hPa';
+
+  // Generate the atmosphere visualization
+  generateAtmosphereVisualization(calculatedPressure);
+
 
 // Call the visualization functions
 generateAtmosphereVisualization();
