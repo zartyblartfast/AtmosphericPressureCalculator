14713d29b48270e4c33af47cff754b39ab95158e
zartyblartfast <zarty_blartfast@yahoo.com>
2023-06-27 21:29:37 +0100

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 // Constants for temperature lapse rates
-export const TROPOPOSPHERE_LAPSE_RATE = -6.5; // in °C/km
+export const TROPOSPHERE_LAPSE_RATE = -6.5; // in °C/km
 export const STRATOSPHERE_LAPSE_RATE = 1; // in °C/km
 export const MESOSPHERE_LAPSE_RATE = -2; // in °C/km
-export const xMax = 100;
+export const xMax = 100;  
