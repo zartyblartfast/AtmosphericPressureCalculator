9d5848e17e65d9e13a0babce0a6cfbaf45c13615
zartyblartfast <zarty_blartfast@yahoo.com>
2023-06-28 12:44:03 +0100

    Update collapsible.js

--- collapsible.js
+++ collapsible.js
@@ -5,10 +5,10 @@ for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
+    if (content.style.display === "none"){
+      content.style.display = "block";
     } else {
-      content.style.maxHeight = content.scrollHeight + "px";
+      content.style.display = "none";
     } 
   });
 }
