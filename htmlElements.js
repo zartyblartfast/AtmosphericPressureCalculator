c1355bcbe85529d0f8ab050b6d0a70b3a5b37d01
zartyblartfast <zarty_blartfast@yahoo.com>
2023-07-02 15:18:55 +0100

    Update htmlElements.js

--- htmlElements.js
+++ htmlElements.js
@@ -1,6 +1,15 @@
-export const seaLevelPressureInput = document.getElementById('sea-level-pressure');
-export const altitudeInput = document.getElementById('altitude');
-export const standardTemperatureInput = document.getElementById('standard-temperature');
+//export const seaLevelPressureInput = document.getElementById('sea-level-pressure');
+//export const altitudeInput = document.getElementById('altitude');
+//export const standardTemperatureInput = document.getElementById('standard-temperature');
+
+//export const seaLevelPressureValue = document.getElementById('sea-level-pressure-number');
+//export const altitudeValue = document.getElementById('altitude-number');
+//export const standardTemperatureValue = document.getElementById('standard-temperature-number');
+
+//export const calculatedPressureOutput = document.getElementById('calculated-pressure');
+export const seaLevelPressureInput = document.getElementById('sea-level-pressure-slider');
+export const altitudeInput = document.getElementById('altitude-slider');
+export const standardTemperatureInput = document.getElementById('standard-temperature-slider');
 
 export const seaLevelPressureValue = document.getElementById('sea-level-pressure-number');
 export const altitudeValue = document.getElementById('altitude-number');
